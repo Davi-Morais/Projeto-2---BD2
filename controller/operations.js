a2f372104ad23a59cb302e1ebe6a97188cb7246a
Davi Morais <davimorais847@gmail.com>
2023-01-17 18:26:17 -0300

    operacao de inserir anotacoes criada

--- controller/operations.js
+++ controller/operations.js
@@ -1,6 +1,7 @@
 const client = require('../database/database');
 
-const buscaTodo = async ()=> {
+//busca todos os documentos no banco
+const buscaTudo = async ()=> {
     try {
         await client.connect();
         const db = client.db(process.env.DB_NAME);
@@ -16,13 +17,13 @@ const buscaTodo = async ()=> {
 }
 
 //busca por texto
-const busca_textual = async () => {
+const busca_textual = async (pesquisa) => {
     try {
         await client.connect();
-        const db = client.db('banco-deAnotacoes');
-        const anotacoes = db.collection('Anotacoes');
+        const db = client.db(process.env.DB_NAME);
+        const anotacoes = db.collection(process.env.COLLECTION_NAME);
 
-        const resposta = await anotacoes.find({ $text: { $search: "segunda" } }).toArray();
+        const resposta = await anotacoes.find({ $text: { $search: pesquisa } }).toArray();
         resposta.forEach(result => {
             console.log(result);
         })
@@ -31,4 +32,23 @@ const busca_textual = async () => {
     }
 }
 
-busca_textual();
+//inserir documento
+//Tem que receber um objeto
+const inserir_documento = async (anotacao)=> {
+    try {
+        await client.connect();
+        const db = client.db(process.env.DB_NAME);
+        const anotacoes = db.collection(process.env.COLLECTION_NAME);
+
+        await anotacoes.insertOne({ titulo: anotacao.titulo, conteudo: anotacao.conteudo });
+    }finally {
+        client.close();
+    }
+}
+const anotacao = {
+    titulo: "Novos jogos", 
+    conteudo: "Muitos rpgs novos em 2023, hein?"
+}
+inserir_documento(anotacao);
+
+module.exports = {buscaTudo, busca_textual};
