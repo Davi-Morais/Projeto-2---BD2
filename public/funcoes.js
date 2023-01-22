8905af901a167a33847dc054aee535cc9a375b16
Davi Morais <davimorais847@gmail.com>
2023-01-22 11:17:06 -0300

    estilo do form criado

--- public/funcoes.js
+++ public/funcoes.js
@@ -9,20 +9,21 @@ async function pegar_todas_anotacoes(){
 
 async function carregar_todas_as_anotacoes() {
     const res = await pegar_todas_anotacoes();
-
+    console.log(res);
     const div_conteiner = document.getElementById('all_anotacoes');
+        
+        //for usado para imprimir as anotacoes na tela:
+        res.forEach(element => {
+        const titulo = element.titulo;
+        const conteudo = element.conteudo;
 
-    //for usado para imprimir as anotacoes na tela:
-    res.forEach(element => {
-       const titulo = element.titulo;
-       const conteudo = element.conteudo;
+        const div_anotacoes = document.createElement("div");
+        div_anotacoes.className = 'anotacao';
+        div_anotacoes.innerHTML += `<h3>${titulo}</h3><p>${conteudo}</p>`; 
 
-       const div_anotacoes = document.createElement("div");
-       div_anotacoes.className = 'anotacao';
-       div_anotacoes.innerHTML += `<h3>${titulo}</h3><p>${conteudo}</p>`; 
-
-       div_conteiner.appendChild(div_anotacoes);
+        div_conteiner.appendChild(div_anotacoes);
     });
+
 }
 
 carregar_todas_as_anotacoes();
