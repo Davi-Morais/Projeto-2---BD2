37bea73efefb38422f407cc708975195117e6ee8
Thalyson1 <thalyson.rian@academico.ifpb.edu.br>
2023-01-25 11:09:19 -0300

    Ajustando Schema

--- connect.js
+++ connect.js
@@ -14,4 +14,31 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Houve um erro ao se conectar com o banco ", err)
 })
 
+//Model - Anotações
+//Definindo Model
+const AnotacaoSchema = mongoose.Schema({
+    titulo: {
+        type: String, 
+        require: true,
+        weights: 2
+    },
+    conteudo:{
+        type: String,
+        require: true,
+        weights: 1
+    }
+})
 
+//Informando a Collection notas
+mongoose.model('notas', AnotacaoSchema)
+
+const newAnotacao = mongoose.model('notas')
+
+new newAnotacao({
+    titulo:"Testando titulo 1",
+    conteudo:"Funcionou?"
+}).save().then(()=>{
+    console.log("Anotação salva com sucesso")
+    }).catch((err)=>{
+        console.log("Houve um erro ao salvar: ", err)
+})
